a6420a6d6edbe8560f6b5df31beefb185bc6bbaf
Ely Deckers <e.deckers@gmail.com>
2022-08-17 12:22:34 +0200

    fix: pin to specific hash instead of master branch

--- fetch-and-patch-original.sh
+++ fetch-and-patch-original.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+COMMIT_HASH=7de971e35b3770028d04b0fb6c6bd54e4c2551a6
+
 echo "Nuking 'dist' directory"
 rm -rf dist
 echo "Nuked 'dist' directory"
@@ -9,7 +11,7 @@ mkdir dist && cd dist
 echo "Created 'dist' and changing workdir"
 
 echo "Retrieving lambda PSql rotator source from AWS respository"
-curl -s https://raw.githubusercontent.com/aws-samples/aws-secrets-manager-rotation-lambdas/master/SecretsManagerRDSPostgreSQLRotationSingleUser/lambda_function.py > lambda_function.py
+curl -s https://raw.githubusercontent.com/aws-samples/aws-secrets-manager-rotation-lambdas/${COMMIT_HASH}/SecretsManagerRDSPostgreSQLRotationSingleUser/lambda_function.py > lambda_function.py
 echo "Retrieved lambda PSql rotator source from AWS respository"
 
 echo "Patching original code to use psycopg2"
